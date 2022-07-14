13c196934981dc020d1e0685a28be412b8b67166
Isis Zapata <isis.zapata.hn@gmail.com>
2022-07-14 14:25:38 -0600

    Update App.js

--- twitter-clon-isis/src/App.js
+++ twitter-clon-isis/src/App.js
@@ -5,8 +5,8 @@ function App() {
     <div className='flex min-h-screen max-w-7xl mx-auto border'>
     
       <div className='w-72 bg-blue-200'>Slidebar</div>
-      <main className='bg-purple-200'>Feed</main>
-      <aside className='bg-green-200'>Timeline</aside>
+      <main className='flex-1 bg-purple-200'>Feed</main>
+      <aside className='bg-green-200 w-80'>Timeline</aside>
 
     </div>
   );
