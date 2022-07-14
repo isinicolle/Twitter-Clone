9ea0ec90448ac9f69a3df21c25a78d4618d507b6
Isis Zapata <isis.zapata.hn@gmail.com>
2022-07-14 14:20:21 -0600

    Update App.js
    .

--- twitter-clon-isis/src/App.js
+++ twitter-clon-isis/src/App.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 function App() {
   return (
-    <div className='min-h-screen max-w-7xl mx-auto bg-black'>
-      
+    <div className='flex min-h-screen max-w-7xl mx-auto border'>
+    
+      <div className='w-72 bg-blue-200'>Slidebar</div>
+      <main className='bg-purple-200'>Feed</main>
+      <aside className='bg-green-200'>Timeline</aside>
+
     </div>
   );
 }
