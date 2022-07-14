9bfe273b0c9675ffb43870d56c61c2daba576d03
Isis Zapata <isis.zapata.hn@gmail.com>
2022-07-14 17:05:58 -0600

    .
    .

--- twitter-clon-isis/src/layout/Sidebar.js
+++ twitter-clon-isis/src/layout/Sidebar.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { HomeIcon } from '../icons/icon';
+
 
 const Sidebar = () => {
   return (
     <div className='w-72 bg-blue-200'>
-        Sidebar
+       <HomeIcon/>
         </div>
   )
 }
