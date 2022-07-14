a4a9d5897c957f99286971d0aaa39658bf023b05
Isis Zapata <isis.zapata.hn@gmail.com>
2022-07-14 12:49:34 -0600

    Update index.css
    Update index.css

--- twitter-clon-isis/src/App.js
+++ twitter-clon-isis/src/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1 className='bg-primary-base'>App</h1>
+      <h1 className='bg-black w-1265'>
+        App
+        </h1>
     </div>
   );
 }
