acdce9fc9a05cf5252d25a93797c0d39fdb75948
Isis Zapata <isis.zapata.hn@gmail.com>
2022-07-14 12:06:34 -0600

    Update tailwind.config.js

--- twitter-clon-isis/tailwind.config.js
+++ twitter-clon-isis/tailwind.config.js
@@ -5,10 +5,18 @@ module.exports = {
     extend: {
       colors: {
         primary: {
-          base: '#00acee',
-          light: '#00acee',
-          dark: '#00acee',
+          base: 'hsl(203,89%,53%)',
+          light: 'hsl(203,89%,46%)',
+          dark: 'hsl(203,89%,96%)',
           },
+          gray:{
+            dark: 'hsl(203,89%, 96%)',
+            light: 'hsl(203,89%,96%)',
+            extraLight: 'hsl(203,89%, 96%)',
+            Lightest: 'hsl(203,89%, 96%)',
+
+          },
+          black: '#14171A',
           },
     },
   },
