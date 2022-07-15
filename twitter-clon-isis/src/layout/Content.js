2f1f0eed1db07058e1c5e4668aabc1c7e8a3012e
Isis Zapata <isis.zapata.hn@gmail.com>
2022-07-15 01:15:16 -0600

    Update Content.js

--- twitter-clon-isis/src/layout/Content.js
+++ twitter-clon-isis/src/layout/Content.js
@@ -1,12 +1,20 @@
-import React, {useEffect} from 'react';
-import db from "../db";
-
+import React from 'react'
+import { PopulerIcon } from '../icons/icon'
 
 const Content = () => {
   return (
     <main className='flex-1 bg-purple-200'>
-      Feed
-      </main>
+      <header>
+        <span>
+          <h1>
+            Content
+            <PopulerIcon 
+            className='w-6 h-6 text-primary-base'
+            />
+            </h1>
+        </span>
+      </header>
+    </main>
   )
 }
 
