fdccfa7dd781c35f9dcdadec842c5d2c3dd74094
Isis Zapata <isis.zapata.hn@gmail.com>
2022-07-15 01:19:53 -0600

    Update Content.js

--- twitter-clon-isis/src/layout/Content.js
+++ twitter-clon-isis/src/layout/Content.js
@@ -4,15 +4,15 @@ import { PopulerIcon } from '../icons/icon'
 const Content = () => {
   return (
     <main className='flex-1 bg-purple-200'>
-      <header>
-        <span>
+      <header className='flex justify-between items-center
+      p-4 border-b border-gray-extraLight'>
+        <span className='font-bold text-x text-gray-900'> Home</span>
           <h1>
-            Content
             <PopulerIcon 
             className='w-6 h-6 text-primary-base'
             />
             </h1>
-        </span>
+       
       </header>
     </main>
   )
