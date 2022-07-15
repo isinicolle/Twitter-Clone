c78cfb8751546cd50177fbe7d84cc8abeac6ac9c
Isis Zapata <isis.zapata.hn@gmail.com>
2022-07-15 00:03:42 -0600

    Update Widgets.js

--- twitter-clon-isis/src/layout/Widgets.js
+++ twitter-clon-isis/src/layout/Widgets.js
@@ -4,10 +4,20 @@ import { SearchIcon } from '../icons/icon'
 const Widgets = () => {
   return (
     <aside className='w-80'>
-      <div className='flex items-center space-x-4 p-3 m-3 bg-gray-200 rounded-full text-gray-dark'>
+      <div className='flex 
+      items-center 
+      space-x-4 
+      p-3 m-3
+       bg-gray-200 
+       rounded-full
+        text-gray-dark
+        focus-within:bg-white
+        focus-within:ring-l 
+        focus-within:ring-blue-500
+        '>
         <SearchIcon className='w-5 h-5' />
         <input type='text' 
-        className='placeholder-gray-dark bg-transparent outline-none w-full text-sm' 
+        className='placeholder-gray-dark bg-transparent focus:outline-none w-full text-sm' 
         placeholder='Search Twitter' />
       </div>
       <div>Timeline</div>
