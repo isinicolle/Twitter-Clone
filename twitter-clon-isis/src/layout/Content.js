0f29d0383878466776bcbce1d0f09a0c0e14d699
Isis Zapata <isis.zapata.hn@gmail.com>
2022-07-15 20:11:25 -0600

    Update Content.js

--- twitter-clon-isis/src/layout/Content.js
+++ twitter-clon-isis/src/layout/Content.js
@@ -18,8 +18,8 @@ const Content = () => {
 
   return (
     <main className='flex-1 flex flex-col border-r border-l border-gray'>
-      <header className='sticky top-0 z-10 bg-white flex justify-between items-center
-      p-4 border-b border-gray-extraLight bg-white'>
+      <header className='sticky top-0 z-10 bg-Lightest flex justify-between items-center
+      p-4 border-b border-gray-extraLight bg-Lightest'>
         <span className='font-bold text-x text-gray-900'> Home</span>
           <h1>
             <PopulerIcon 
